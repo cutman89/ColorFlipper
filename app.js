7b2322a060df92e582de60435d8d96d7b29f9d4e
cutman89 <42938991+cutman89@users.noreply.github.com>
2022-05-01 18:05:50 +0100

    Add files via upload
    update with message to accept cookie

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const storageType = cookiestorage;
 const consentPropertyName = "jdc_consent";
 const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
 const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
 //event
 btn.addEventListener("click", changeColor);
 
@@ -44,10 +45,17 @@ function changeColor(evt) {
 colors.innerHTML = localStorage.getItem("color");
 document.body.style.backgroundColor = localStorage.getItem("color");
 window.onload = () => {
-  if (shouldShowPopup()) {
-    const consent = confirm("Agree to the terms and conditions of the site");
-    if (consent) {
-      saveToStorage();
-    }
+  const acceptFn = (event) => {
+    saveToStorage(storageType);
+    consentPopup.classList.add("hidden");
+  };
+  const consentPopup = document.getElementById("consent-popup");
+  const acceptBtn = document.getElementById("accept");
+  acceptBtn.addEventListener("click", acceptFn);
+
+  if (shouldShowPopup(storageType)) {
+    setTimeout(() => {
+      consentPopup.classList.remove("hidden");
+    }, 2000);
   }
 };
